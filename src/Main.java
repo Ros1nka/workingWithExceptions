e9eb611ea306b5657dc7760cf93e1e5cc5a918d0
Евгений Страхов <goodwin.s@mail.ru>
2024-09-19 06:45:45 +0300

    HW

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
+
+    }
+
+    public static void registration(String login, String password, String confirmPassword) {
+        Pattern pattern = Pattern.compile("^a-ZA-Z_0-9");
+        Matcher matcher = pattern.matcher(password);
 
     }
 }
