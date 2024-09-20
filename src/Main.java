3257d017f55a69be079b5e33bf526dc5476ee9f3
Eugeny <goodwin.s@mail.ru>
2024-09-20 10:08:36 +0300

    hw

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,37 @@
+import Exceptions.WrongLoginException;
+import Exceptions.WrongPasswordException;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
+        String login = "rwfDDg45gf";
+        String password = "sfeb_rewf1235";
+        String confirmPassword = "sfeb_rewf1235";
 
+        try {
+            registration(login, password, confirmPassword);
+        } catch (WrongLoginException e) {
+            System.out.println("Ошибка логина");
+        } catch (WrongPasswordException e) {
+            System.out.println("Ошибка пароля");
+        }
     }
 
     public static void registration(String login, String password, String confirmPassword) {
-        Pattern pattern = Pattern.compile("^a-ZA-Z_0-9");
-        Matcher matcher = pattern.matcher(password);
+        if (!compliance(login)) {
+            throw new WrongLoginException();
+        }
+        if (!compliance(password) || !password.equals(confirmPassword)) {
+            throw new WrongPasswordException();
+        }
+    }
+  
+    public static boolean compliance(String item) {
+        Pattern pattern = Pattern.compile("[^a-zA-Z0-9_]");
+        Matcher matcher = pattern.matcher(item);
 
+        return item.length() <= 20 && !matcher.find();
     }
 }
