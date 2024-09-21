8f6d11843886f954a23e90b6bd364e651a55c999
Евгений Страхов <goodwin.s@mail.ru>
2024-09-21 17:54:29 +0300

    HW

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        String login = "rwfDDg45gf";
+        String login = "r213wf12345671234DDg";
         String password = "sfeb_rewf1235";
         String confirmPassword = "sfeb_rewf1235";
 
@@ -20,18 +20,27 @@ public class Main {
     }
 
     public static void registration(String login, String password, String confirmPassword) {
-        if (!compliance(login)) {
-            throw new WrongLoginException();
-        }
-        if (!compliance(password) || !password.equals(confirmPassword)) {
-            throw new WrongPasswordException();
-        }
+        compliance(login);
+//        if (!compliance(login)) {
+//            throw new WrongLoginException();
+//        }
+//        if (!compliance(password) || !password.equals(confirmPassword)) {
+//            throw new WrongPasswordException();
+//        }
     }
-  
-    public static boolean compliance(String item) {
-        Pattern pattern = Pattern.compile("[^a-zA-Z0-9_]");
-        Matcher matcher = pattern.matcher(item);
 
-        return item.length() <= 20 && !matcher.find();
+    public static void compliance(String item) {
+        Pattern pattern = Pattern.compile("^[a-zA-Z0-9_]{0,20}$");
+        Matcher matcher = pattern.matcher(item);
+        while (matcher.find()) {
+            System.out.println("Найдено совпадение " + item.substring(matcher.start(),matcher.end()) + " с "+ matcher.start() + " по " + (matcher.end()-1) + " позицию");
+        }
     }
+
+//    public static boolean compliance(String item) {
+//        Pattern pattern = Pattern.compile("[^a-zA-Z0-9_]");
+//        Matcher matcher = pattern.matcher(item);
+//
+//        return item.length() <= 20 && !matcher.find();
+//    }
 }
